2b09bf532206927845af2c932808d643cd840015
Subm1s <82820375+Subm1s@users.noreply.github.com>
2021-11-12 10:23:54 +0200

    Update Section.jsx

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,4 +1,5 @@
 import s from './Section.module.css';
+import {PropTypes} from 'prop-types';
 
 const Section = ({ title, children }) => {
     return (
@@ -8,5 +9,9 @@ const Section = ({ title, children }) => {
       </div>
     );
   };
+
+  Section.propTypes = {
+    title: PropTypes.string,
+  }
   
   export default Section;
